ef4e836e4ebe9e47fa9b551562afba9b682aeefa
KinCod <100110821+KinCod@users.noreply.github.com>
2024-02-15 18:47:23 +0530

    Added Login and Authentication Section(inc. MiddleWare)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ const express = require("express");
 //input the User.js schema from the models of mongoose to use it here
 const User = require("../models/User");
 const router = express.Router();
+const fetchUser = require("../middleware/fetchUser");
 
 //using validation
 const { body, validationResult } = require("express-validator");
@@ -35,7 +36,7 @@ const { body, validationResult } = require("express-validator");
 //With validation
 //iss post request ka naam hi LOGIN hai
 router.post(
-  "/login",
+  "/signin",
   [body("name").notEmpty(), body("password").isLength({ min: 6 })],
   async (req, res) => {
     const result = validationResult(req);
@@ -44,51 +45,116 @@ router.post(
       //agar api ne jo diya vo validation ko follow krra tab enter kro
       //store to db
 
-      try{
+      try {
         //finding if particular email is already in database
-          let us = await User.findOne({ email: req.body.email }); //finding User model vich ki if jo input body mai hai api k vih same db mai hai k nhi
-          //jo ye User hai this is the DB and ismai hum dhundrhe if email exists or no
+        let us = await User.findOne({ email: req.body.email }); //finding User model vich ki if jo input body mai hai api k vih same db mai hai k nhi
+        //jo ye User hai this is the DB and ismai hum dhundrhe if email exists or no
 
-          //agar hai db mai tab we send a bad request to the backend
-          if (us) {
-            return res
-              .status(400)
-              .json({ errors: { msg: "Email already exists." } });
-          }
-          //agar nhi hai db mai toh store krlo
+        //agar hai db mai tab we send a bad request to the backend
+        if (us) {
+          return res
+            .status(400)
+            .json({ errors: { msg: "Email already exists." } });
+        }
+        //agar nhi hai db mai toh store krlo
 
-          //idhar were using the hasing of bycryptjs
-          // 1. added salt
-          // 2.Merged salt with the password that was sent from the body of server and then together hashed
-          const salt = await bcrypt.genSaltSync(10);
-          secPass = await bcrypt.hash(req.body.password, salt); //password with added salt and then hashed, returns a promise that used await
+        //idhar were using the hasing of bycryptjs
+        // 1. added salt
+        // 2.Merged salt with the password that was sent from the body of server and then together hashed
+        const salt = await bcrypt.genSaltSync(10);
+        console.log(salt);
+        secPass = await bcrypt.hash(req.body.password, salt); //password with added salt and then hashed, returns a promise that used await
 
-          //3. This hashed password is now sent to db(ab authentication k time again same process hogi and db wala hashed and jo user ne pass bhara uska has compare hoga and then theyll login only if both hashes are identical)
-          us = await User.create({
-            //jo bhi aaya api se usko store kro as json into db following the user schema
-            name: req.body.name,
-            email: req.body.email,
-            password: secPass,
-          });
+        //3. This hashed password is now sent to db(ab authentication k time again same process hogi and db wala hashed and jo user ne pass bhara uska has compare hoga and then theyll login only if both hashes are identical)
+        us = await User.create({
+          //jo bhi aaya api se usko store kro as json into db following the user schema
+          name: req.body.name,
+          email: req.body.email,
+          password: secPass,
+        });
 
-          const data = {
-            //this is data that stores and object named user and is sent to jwt
-            user: {
-              id: us.id, //this id is the id for the user created in the data base
-            },
-          };
-          var authtoken = jwt.sign(data, "shhhhh"); //webtoken from data and secret
+        const data = {
+          //this is data that stores and object named user and is sent to jwt
+          user: {
+            id: us.id, //this id is the id for the user created in the data base
+          },
+        };
+        const authtoken = jwt.sign(data, "shhhhh"); //webtoken from data and secret m(returns a promise)
+        //ab as token will be provided to the user so s/he dont have to login again and again
 
-          //res to api
-          return res.send(secPass); //responding authorisation token id to the server
-      }catch(err){
-          console.log(err);
+        //res to api
+        return res.send({ authtoken }); //responding authorisation token id to the server
+        //ye upar as an object return kra but aise kyu ni likhe --> {authtoken : authtoken} (in json format)
+        //this is because as ES6 use krre so directly the js knows ki agar koi var daala hai toh same name k liye data daalo {name} = {name : "data inside name"}
+      } catch (err) {
+        console.log(err);
       }
-      
     }
 
-    res.send({ errors: result.array() });
+    return res.send({ errors: result.array() });
   }
 );
+
+//authenticating a user
+router.post(
+  "/login",
+  [
+    body("password", "Add Password of size atleast 6char").isLength({ min: 6 }),
+    body("email", "Email Does not Exist").isEmail(),
+  ], //name not needed as obv user login hi krra (sign up nhi)
+  async (req, res) => {
+    const result = validationResult(req);
+
+    if (!result.isEmpty()) return res.json({ errors: result.array() });
+
+    const { email, password } = req.body; //data request kra from api (imagined ki joh data page pe user ne input kra to login vohi req kra backend ne from the page)
+
+    try {
+      
+      let user = await User.findOne({email : email}); //ye db mai pass find krke puri row return krdega
+
+      if (!user)
+        return res
+          .status(400)
+          .json({ error: "Please enter Correct Credentials" }); //ye toh if galat input daala
+
+      //if user exist krta tab we check ki password sahi ya nhi
+      //so ye bhi pehle hash hoga salt dlega phir db mai cmpare hoga and then btaega if correct or not
+
+      const passCompare = await bcrypt.compare(password, user.password); //comparing jo entered pass tha and jo DB mai password hai
+      
+      if (!passCompare) return res.status(400).json({ error: "Please enter Correct Credentials" });
+      
+      //ifpass bhi sahi hogya tab bas token phirse bhej denge
+      const data = {
+        user: {id: user.id,}
+      };
+
+      const authtoken = jwt.sign(data, "shhhhh");
+      return res.send({ user });
+    } catch {
+      return res.status(400).json({ error: "Some Internal Error Occured" }); //works on time like when database mai data ho hi naa etc etc
+    }
+  }
+);
+
+//Get logged in user detail
+//so basically using authtoken hum user id nikal lenge and then uske baad uss id se user ka data find kar lenge except pass and then
+// show kardenge user ko
+
+router.post("/getuser", fetchUser ,async (req,res)=>{
+
+    try{
+      userId = req.user.id;
+      const user = await User.findOne(userId).select("-password");  //password k alawa sab select karna
+
+      res.send(user);
+    }
+    catch(err){
+      return res.json({err});
+    }
+    
+  }
+)
 
 module.exports = router;
