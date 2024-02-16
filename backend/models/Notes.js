c1468af2e97d2a83a9e7bc3df2d7957ece8ff15d
KinCod <100110821+KinCod@users.noreply.github.com>
2024-02-16 17:52:48 +0530

    added a userId column thats tells k the note belongs to particular

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,24 +1,27 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
-    user :{
-        type: Schema.Types.ObjectId,            //ab idhar directly user store hojayega
-        ref:'user'                      //dusre model ka naam
-    },
-    title :{
-        type: String,
-        required: true
-    },
-    description:{
-        type:String,
-        unique:true,
-        required : true
-    },
-    date:{
-        type : Date,
-        default : Date.now
-    }
-  });
+  user: {
+    type: mongoose.Schema.Types.ObjectId, //ab idhar directly user store hojayega
+    ref: "user", //dusre model ka naam
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type : String,
+    required : true
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-  module.exports = mongoose.model('notes',NotesSchema); //export a mongoose model as name user
+module.exports = mongoose.model("notes", NotesSchema); //export a mongoose model as name user
