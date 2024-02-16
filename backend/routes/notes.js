c38f453389d6baaf2c43493402001f4b9ae0b96f
KinCod <100110821+KinCod@users.noreply.github.com>
2024-02-16 15:28:09 +0530

    added a router for ->fetching note and for ->adding notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,11 +1,45 @@
 //initially using routes imported from express
 
-const express = require('express')
+const express = require('express');
+const fetchuser = require('../middleware/fetchUser');
 const router = express.Router()
+const { body, validationResult } = require("express-validator");  //using validator
 
-router.get('/',(req,res)=>{
+//importing the notes wala model
+const Notes = require('../models/Notes');
 
-    res.json([]);          //converted into json
+//Route to get all the Notes.
+//idhar we'll again use the MIDDLEWARE to get id of user and then use that id to get the notes
+router.get('/fetchNotes',fetchuser,async (req,res)=>{
+
+    try{
+        const notes = await Notes.find({user: req.user.id}) // finding note with paticular id
+        res.json(notes);          //converted into json
+    }
+    catch(err){
+        return res.status(400).json({msg: "Error Occured"});
+    }
+    
+})
+
+//add notes for user
+//so obv sbse pehle toh userr ko fetch krenge and then uss id k liye notes add krenge
+//so joh bhi note add kra in page vo api accept kregi and phir uss user id ko dekhkar uss particular db mai store kar degi
+router.post('/addNotes',fetchuser,async(req,res)=>{
+
+    try{
+        //aise apna note create hokar DB mai store hojayga and will be following the schema
+        const note =await Notes.create({
+            user : req.user.id,                //got from fetch user
+            title : req.body.title,
+            description : req.body.description
+        })
+
+        res.send({note});
+    }catch(err){
+        return res.status(400).json({msg:"Failed to Add Note!"})
+    }
+    
 })
 
 module.exports = router;
