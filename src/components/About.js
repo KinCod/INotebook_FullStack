596f93131d8cea48b353470743385602f91ffdef
KinCod <100110821+KinCod@users.noreply.github.com>
2024-02-19 23:39:03 +0530

    abhi joh context se ek State and Method  access krre vo ismai implemented

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{ useContext, useEffect } from 'react'
+import NoteContext from '../context/notes/noteContext'
+
+//toh basically udhar se jis jis tag ko context provide krna uss tag ko wrap kra state mai
+//and ab import kra tag k andar context ko
 
 const About = () => {
+  const a = useContext(NoteContext);         //using the context (ab in jsx kuch bhi state karlo access)
+
+  useEffect(() => {           //always runs once on page startUP
+    a.update();      
+  }, [])
+  
   return (
     <div className=''>
-      Tatti khao
+      {a.name.name } is a GOD in class {a.name.class};
     </div>
   )
 }
 
-export default About
+export default About;
 
