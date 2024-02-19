944d7dc6fc4a53ce5fdc61fc864b70de3a8f26be
KinCod <100110821+KinCod@users.noreply.github.com>
2024-02-19 22:39:53 +0530

    Made some components and added basic stuff eg Nav router etc

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className=''>
+      Tatti khao
+    </div>
+  )
+}
+
+export default About
+
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div>
+      GHAR
+    </div>
+  )
+}
+
+export default Home
