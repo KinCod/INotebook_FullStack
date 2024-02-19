aa3c1dbf6b48579f532f49c360eb5a62b8334977
KinCod <100110821+KinCod@users.noreply.github.com>
2024-02-19 23:37:48 +0530

    Just added UseContext ki state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import "./App.css";
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
-    <Router>
-      <NavBar/>
+      <NoteState>
+        <Router>
+          <NavBar />
 
-      <Routes>
-        <Route key={"home"} exact path="/" element={<Home/>}/>
-      </Routes>
+          <Routes>
+            <Route key={"home"} exact path="/" element={<Home />} />
+          </Routes>
 
-      <Routes>
-        <Route key={"about"} exact path="/about" element={<About/>}/>
-      </Routes>
-      
-    </Router>
-      
+          <Routes>
+            <Route key={"about"} exact path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
-)}
+  );
+}
 
 export default App;
