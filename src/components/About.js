3ffac35578da80a4f487eda5afd92d0ed0dc0fb2
KinCod <100110821+KinCod@users.noreply.github.com>
2024-02-20 16:33:37 +0530

    Just remove the Example used for Context Understanding

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,17 @@
-import React,{ useContext, useEffect } from 'react'
-import NoteContext from '../context/notes/noteContext'
+import React, { useContext, useEffect } from "react";
+import NoteContext from "../context/notes/noteContext";
 
 //toh basically udhar se jis jis tag ko context provide krna uss tag ko wrap kra state mai
 //and ab import kra tag k andar context ko
 
 const About = () => {
-  const a = useContext(NoteContext);         //using the context (ab in jsx kuch bhi state karlo access)
+  //const a = useContext(NoteContext);         //using the context (ab in jsx kuch bhi state karlo access)
 
-  useEffect(() => {           //always runs once on page startUP
-    a.update();      
-  }, [])
-  
-  return (
-    <div className=''>
-      {a.name.name } is a GOD in class {a.name.class};
-    </div>
-  )
-}
+  // useEffect(() => {           //always runs once on page startUP
+  //   a.update();
+  // }, [])
+
+  return <div className=""></div>;
+};
 
 export default About;
-
