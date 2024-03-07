66d7a25fd238b3e1325154b56d4d0c41376a53b5
KinCod <100110821+KinCod@users.noreply.github.com>
2024-03-07 22:07:54 +0530

    added an alert method, directly accessing a state from context

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,13 @@ import NoteContext from "../context/notes/noteContext";
 
 const Home = () => {
   const context = useContext(NoteContext);
-  const {vis} = context;
+  const { vis } = context;
   return (
     <>
       <div className="container text-center ">
-        {vis.vis && <Alert message ={vis.alert}/>}       {/*agar vis.vis is true tab alert show kro */}
-        <Notes/>
+        {vis.vis && <Alert message={vis.alert} />}{" "}
+        {/* agar vis.vis is true tab alert show kro */}
+        <Notes />
       </div>
     </>
   );
