7d27e514db58b0abc9941996180db16063f74a0c
KinCod <100110821+KinCod@users.noreply.github.com>
2024-03-08 21:50:18 +0530

    some Changes

--- src/components/Addnote.js
+++ src/components/Addnote.js
@@ -9,7 +9,9 @@ const Addnote = () => {
   const handleSubmit = (e) => {
     //ab obv onchange bhi chal lia and state mai bhi new note ka data store holia ab bas humein context se lia hua add note func run karna hai
     e.preventDefault();        //jo bhi event hai uski default working stop krega(reload ni hoga page on submission)
+    
     addNote(note.title, note.description,note.tag);
+    setNote({title:"",description : "",tag: ""});
   };
 
   //toh we'll make a state and usmai changes done in the input add hojayenge using the onChange method
@@ -40,9 +42,11 @@ const Addnote = () => {
             name="title"
             type="text"
             id="title"
+            minLength={5}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Add 'Title' "
+            placeholder="Add 'Title' (min Length : 5) "
             required
+            value={note.title}
           />
         </div>
         <div className="text-left mb-5">
@@ -57,9 +61,10 @@ const Addnote = () => {
             type="text"
             name="description"
             id="description"
-            placeholder="Description"
+            placeholder="Description (min Length : 5)"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
+            value={note.description}
           />
         </div>
         <div className="text-left mb-5">
@@ -77,12 +82,13 @@ const Addnote = () => {
             placeholder="Enter the TAG"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
+            value={note.tag}
           />
         </div>
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          onClick={handleSubmit}
+          onClick={handleSubmit} disabled = {note.title.length<5 || note.description.length<5}
         >
           Add Note
         </button>
