63ef3623c3fa6a8facf9077bc814b553aeecf71d
KinCod <100110821+KinCod@users.noreply.github.com>
2024-03-09 11:54:41 +0530

    component linked to login button and login api

---
+++ src/components/Login.js
@@ -0,0 +1,70 @@
+"use client";
+
+import { Button, Label, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+
+const Login = () => {
+  const host = "http://localhost:5000/";
+
+  //⁡⁢⁣⁣𝗦𝘁𝗼𝗿𝗶𝗻𝗴 𝘁𝗵𝗲 𝘃𝗮𝗹𝘂𝗲𝘀⁡ from form into a ​‌‍‌⁡⁢⁣⁣𝘀𝘁𝗮𝘁𝗲⁡​
+  const [logi, setLogi] = useState({ email: "", password: "" }); //store Email and pass in this
+  const onChange = (e) => {
+    setLogi({ ...logi, [e.target.name]: [e.target.value] });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    //Fetching the api and Sending Login Data to it
+    const url = `${host}api/auth/login/`;
+
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email: logi.email, password: logi.password}),
+    });
+
+    const json = await response.json();
+    if(json.success){
+        //redirect to notes page
+    }else{
+        window.prompt(json.error);
+    }
+  };
+
+  return (
+    <div>
+      <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmit}>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="email1" value="Your email" />
+          </div>
+          <TextInput
+            id="email1"
+            name="email"
+            type="email"
+            placeholder="name@flowbite.com"
+            required
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="password1" value="Your password" />
+          </div>
+          <TextInput
+            id="password1"
+            name="password"
+            onChange={onChange}
+            type="password"
+            required
+          />
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
