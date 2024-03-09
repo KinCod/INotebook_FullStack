c68194a51ceab8de6bfadcb1391d287b96cd7b1e
KinCod <100110821+KinCod@users.noreply.github.com>
2024-03-09 11:53:32 +0530

    added a route for login and signup components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -17,6 +19,11 @@ function App() {
             <Route key={"home"} exact path="/" element={<Home />} />
             
             <Route key={"about"} exact path="/about" element={<About />} />
+
+            <Route key={"login"} exact path="/login" element={<Login />} />
+
+            <Route key={"signup"} exact path="/signup" element={<SignUp />} />
+
           </Routes>
         </Router>
       </NoteState>
