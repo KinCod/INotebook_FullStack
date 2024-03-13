0716afb1097ba277791aac4b79d7ef9a2dd2aad4
KinCod <100110821+KinCod@users.noreply.github.com>
2024-03-13 18:26:54 +0530

    changes for build

---
+++ backend/api/index.js
@@ -0,0 +1,31 @@
+const connectToMongo = require("../db.js");
+
+connectToMongo();
+
+const express = require("express");
+const app = express();
+const port = 5000;
+
+var cors = require('cors')
+app.use(cors(
+  {
+    origin :["https://deplpy-mern-1whq.vercel.app"],
+    methods:['POST',"GET","DELETE"],
+    credentials : true
+  }
+))
+
+app.use(express.json())       //this acts as middleware bw the api and our application
+
+app.get("/", (req, res) => {
+  res.status(200).json({message:"Hello Doston!"});
+});
+
+//available routes
+app.use('/api/notes' , require('../routes/notes.js'));   //idhar /api/notes se hum is json ko access kar skte
+app.use('/api/auth' ,require('../routes/auth.js'))  //inko endpoint bolte (eg. ye hai ep for ./routes/auth.js)
+
+app.listen(port, () => {
+  console.log(`iNotebook Backend listening on port ** http://localhost:${port}/ **`);
+});
+ 
