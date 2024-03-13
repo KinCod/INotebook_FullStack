fc4709a82a3a206856fbde54f523fdbceab0f73a
KinCod <100110821+KinCod@users.noreply.github.com>
2024-03-13 15:19:34 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
 import "./App.css";
 
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
-import NoteState from "./context/notes/NoteState";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
+import Aalert from "./components/Aalert";
 
 function App() {
+  const [alert, setAlert] = useState({});
+  const showAlert = (message, type) => {
+    setAlert({ message: message, type: type });
+    setTimeout(() => {
+      setAlert({});
+    }, 3000);
+  };
+
   return (
     <>
-      <NoteState>
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route key={"home"} exact path="/home" element={<Home />} />
-            
-            <Route key={"about"} exact path="/about" element={<About />} />
+      <Router>
+        <NavBar showAlert={showAlert}/>
+        <Aalert alert={alert} />
+        <Routes>
+          <Route
+            key={"home"}
+            exact
+            path="/"
+            element={<Home showAlert={showAlert} />}
+          />
 
-            <Route key={"login"} exact path="/" element={<Login />} />
+          <Route key={"about"} exact path="/about" element={<About />} />
 
-            <Route key={"signup"} exact path="/signup" element={<SignUp />} />
+          <Route
+            key={"login"}
+            exact
+            path="/login"
+            element={<Login showAlert={showAlert} />}
+          />
 
-          </Routes>
-        </Router>
-      </NoteState>
+          <Route key={"signup"} exact path="/signup" element={<SignUp showAlert={showAlert}/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NoteState from './context/notes/NoteState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NoteState>
     <App />
+    </NoteState>
   </React.StrictMode>
 );
 
